64fc8dd75a2ee1e076a1c60796754f1192b6d373
wnsdnn <a24369029@gmail.com>
2024-07-10 23:35:13 +0900

    04 기능 분리하기

---
+++ FlutterProjects/image_search/lib/data/api.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:image_search/model/photo_model.dart';
+import 'package:http/http.dart';
+
+class PixabayApi {
+  final baseUrl = 'https://pixabay.com/api/';
+  final key = '42997894-f5e29dd70ea74c8f2c2c3767c';
+
+  Future<List<PhotoModel>> fetch(String query) async {
+    List<PhotoModel> result = [];
+    final url =
+        Uri.parse('$baseUrl?key=$key&q=$query&image_type=photo&pretty=true');
+    final response = await get(url);
+
+    if (response.statusCode == 200) {
+      final body = jsonDecode(response.body);
+      Iterable hits = body['hits'];
+
+      result = hits.map((json) => PhotoModel.fromJson(json)).toList();
+    }
+
+    return result;
+  }
+}
--- FlutterProjects/image_search/lib/screen/home_screen.dart
+++ FlutterProjects/image_search/lib/screen/home_screen.dart
@@ -1,10 +1,8 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
+import 'package:image_search/data/api.dart';
 import 'package:image_search/model/photo_model.dart';
 import 'package:image_search/screen/component/photo_widget.dart';
 import 'package:image_search/screen/layout/default_layout.dart';
-import 'package:http/http.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,23 +13,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _controller = TextEditingController();
+  final api = PixabayApi();
   List<PhotoModel> _photos = [];
-
-  Future<List<PhotoModel>> fetch(String query) async {
-    List<PhotoModel> result = [];
-    final url = Uri.parse(
-        'https://pixabay.com/api/?key=42997894-f5e29dd70ea74c8f2c2c3767c&q=$query&image_type=photo&pretty=true');
-    final response = await get(url);
-
-    if (response.statusCode == 200) {
-      final body = jsonDecode(response.body);
-      Iterable hits = body['hits'];
-
-      result = hits.map((json) => PhotoModel.fromJson(json)).toList();
-    }
-
-    return result;
-  }
 
   @override
   void dispose() {
@@ -68,8 +51,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 suffixIcon: IconButton(
                   onPressed: () async {
                     final keyword = _controller.text;
-                    final photos = await fetch(keyword);
-                    print(photos);
+                    final photos = await api.fetch(keyword);
 
                     setState(() {
                       _photos = photos;
